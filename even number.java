37a0f9bbdb70290ed8542b2390e432fdfe9613d9
Suprazha27 <suprazhamadhu@gmail.com>
2025-07-31 11:28:06 +0530

    Create even number.java

---
+++ even number.java
@@ -0,0 +1,10 @@
+public class EvenNumbersWhileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 50){
+        if(i%2 == 0){
+        System.out.print(i+" ");
+    }
+    i++;
+}}}
+2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 
