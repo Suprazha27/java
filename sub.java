e1d52d464b1b6c250ba9e1b5771179c4e58bf672
Suprazha27 <suprazhamadhu@gmail.com>
2025-07-31 10:09:58 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,17 @@
+package PSNA;
+public class methoddemo {
+    int add(int a, int b) {
+        return b-a;
+    }
+    public static void main(String[] args) {
+        System.out.println("PSNA");
+        methoddemo obj=new methoddemo();
+        System.out.println(obj.add(10, 20));
+    }
+}
+
+PSNA
+10
+
+
+
