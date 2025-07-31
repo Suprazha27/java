6fd51bf1b84abfb5a696c72a070d4f313030f734
Suprazha27 <suprazhamadhu@gmail.com>
2025-07-31 10:08:46 +0530

    Create addition 2.java

---
+++ addition 2.java
@@ -0,0 +1,12 @@
+Addition (method 2)
+
+public class UserInputExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}
+
+Output
+30
