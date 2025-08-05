98ddb83dcfe8eb5f8d92a74934d2858765466829
Suprazha27 <suprazhamadhu@gmail.com>
2025-08-05 14:08:42 +0530

    Create teodarry.java

---
+++ teodarry.java
@@ -0,0 +1,14 @@
+public class teodarray {
+    public static void main(String[] args) {
+        int [][] arr= {{1,1,1},{0,0,0},{2,2,2}};
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr.length;j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+111
+000
+222
